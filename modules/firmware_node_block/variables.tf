dc0436ecaeee41971174e92ac18ed7b1e5a33ed9
scotttyso <tyscott@cisco.com>
2021-03-10 17:52:20 -0700

    fixed order in variables

--- modules/firmware_node_block/variables.tf
+++ modules/firmware_node_block/variables.tf
@@ -8,18 +8,18 @@ variable "node_firmware_block" {
     annotation        = optional(string)
     description       = optional(string)
     firmware_group_dn = optional(string)
+    name_alias        = optional(string)
     node_id_from      = optional(number)
     node_id_to        = optional(number)
-    name_alias        = optional(string)
   }))
   default = {
     default = {
       annotation        = ""
       description       = ""
       firmware_group_dn = ""
+      name_alias        = ""
       node_id_from      = 201
       node_id_to        = 201 # If the node_id_to is left blank then the node_id_from will be used for the node_id_to as well.
-      name_alias        = ""
     }
   }
 }
@@ -30,9 +30,9 @@ locals {
       annotation        = (v.annotation != null ? v.annotation : "")
       description       = (v.description != null ? v.description : "")
       firmware_group_dn = (v.firmware_group_dn != null ? v.firmware_group_dn : "")
+      name_alias        = (v.name_alias != null ? v.name_alias : "")
       node_id_from      = coalesce(v.node_id_from, 201)
       node_id_to        = coalesce(v.node_id_to, v.node_id_from, 201)
-      name_alias        = (v.name_alias != null ? v.name_alias : "")
     }
   }
 }
